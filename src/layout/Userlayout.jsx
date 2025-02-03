7e61a2da966a690fc332ac40a5cd84529f0bc504
jishnum <jishnuum2000@gmail.com>
2025-02-03 19:27:30 +0530

    change

--- src/layout/Userlayout.jsx
+++ src/layout/Userlayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Header from '../Components/Header'
+import Header from '../components/Header'
 import Footer from '../Components/Footer'
 
 function Userlayout() {
